251a8855c6cc78f7302783bd2faab4610d1464d5
kijiwon <119961147+kijiwon@users.noreply.github.com>
2023-04-04 22:41:07 +0900

    프로필사진&깃헙주소 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import TeamPage from './page/TeamPage';
 
 function App() {
     return (
         <div className="App">
-            <div>여기는 일단 냅두기 영역</div>
+            {/* <div>여기는 일단 냅두기 영역</div> */}
+            <TeamPage/>
         </div>
     );
 }
--- src/page/TeamPage.js
+++ src/page/TeamPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import styled from 'styled-components';
+import profileKI from './../image/지원님프로필.jpg';
+import profileLEE from './../image/현진님프로필.png';
+import profileCHO from './../image/지현님프로필.png';
+import profileCHOI from './../image/예슬님프로필.jpg';
 
-let 팀페이지 = styled.div`
+let TeamPageCompo = styled.div`
+    margin-top: 65px;
+    background-color: white;
     width: 100vw;
-    height: 500px;
+    height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -11,9 +17,15 @@ let 팀페이지 = styled.div`
     padding-top: 50px;
     ul{
         list-style: none;
-        display: grid;
+        font-size: 20px;
+        /* display: grid;
         grid-template-columns: repeat(2,1fr);
-        grid-gap: 40px;
+        grid-gap: 40px; */
+        display: flex;
+        flex-direction: row;
+        justify-contents: space-between;
+        align-items : center;
+        
     }
     li{
         width: 280px;
@@ -26,41 +38,74 @@ let 팀페이지 = styled.div`
         padding-top: 10px;
     }
     .img{
-        width: 60px;
-        height: 60px;
-        border: 1px solid black;
+        width: 100px;
+        height: 100px;
+        /* border: 1px solid black; */
         border-radius: 50%;
         text-align: center;
-        background-color: aliceblue;
+        background-size: cover;
+        background-position : center;
+        background-repeat: no-repeat;
+        margin-bottom: 14px;
+    }
+  
+    .ki{
+        background-image: url(${profileKI});
+    }
+    .lee{
+        background-image: url(${profileLEE});
+        background-size : contain;
+    }
+    .cho{
+        background-image: url(${profileCHO});
+        background-size: contain;
+    }
+    .choi{
+        background-image: url(${profileCHOI});
+    }
+    .name{
+        font-size: 26px;
     }
 `;
 function TeamPage(){
     
     return (
-        <팀페이지>
+        <TeamPageCompo>
             <ul>
                 <li>
-                    <div className='img'></div>
-                    <p>기지원</p>
-                    <a href='/'><p>깃헙 주소</p></a>
+                    <div className='img ki'></div>
+                    <p className='name'>기지원</p>
+                    <div className='desc'>
+                        <p>담당 컴포넌트 : Team Page</p>
+                        <p>Github URL : <a target="_blank" rel="noreferrer" href='https://github.com/lhj5924/UcleLupin/tree/TCTZ-11--teamPage'>'UcleLupin/TeamPage'</a></p>
+                    </div>
                 </li>
                 <li>
-                    <div className='img'></div>
-                    <p>팀원 2</p>
-                    <a href='/'><p>깃헙 주소</p></a>
+                    <div className='img lee'></div>
+                    <p className='name'>이현진</p>
+                    <div className='desc'>
+                        <p>담당 컴포넌트 : List</p>
+                        <p>Github URL : <a target="_blank" rel="noreferrer" href='https://github.com/lhj5924/UcleLupin/tree/TCTZ-10--feature-list'>'UcleLupin/List'</a></p>
+                    </div>
                 </li>
                 <li>
-                    <div className='img'></div>
-                    <p>팀원 3</p>
-                    <a href='/'><p>깃헙 주소</p></a>
+                    <div className='img cho'></div>
+                    <p className='name'>조지현</p>
+                    <div className='desc'>
+                        <p>담당 컴포넌트 : Main</p>
+                        <p>Github URL : <a target="_blank" rel="noreferrer" href='https://github.com/lhj5924/UcleLupin/tree/TCTZ-9--feature-main'>'UcleLupin/Main'</a></p>
+                    </div>
                 </li>
                 <li>
-                    <div className='img'></div>
-                    <p>팀원 4</p>
-                    <a href='/'><p>깃헙 주소</p></a>
+                    <div className='img choi'></div>
+                    <p className='name'>최예슬</p>
+                    <div className='desc'>
+                        <p>담당 컴포넌트 : Header&Footer</p>
+                        <p>Github URL : <a target="_blank" rel="noreferrer" href='https://github.com/lhj5924/UcleLupin/tree/TCTZ-8--header-footer'>'UcleLupin/Header&Footer'</a></p>
+                    </div>
                 </li>
             </ul>
-        </팀페이지>
+        </TeamPageCompo>
     )
 }
 
