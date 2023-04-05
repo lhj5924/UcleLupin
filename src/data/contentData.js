cc15611007f03985d4d4368f12303fb70d15607a
chochojj <csgy114@gmail.com>
2023-04-05 23:04:19 +0900

    더미데이터 파일 생성

---
+++ src/data/contentData.js
@@ -0,0 +1,19 @@
+const contentData = [
+    {
+      title: "Example 1",
+      image: "https://via.placeholder.com/150",
+      link: "https://example.com/1"
+    },
+    {
+      title: "Example 2",
+      image: "https://via.placeholder.com/150",
+      link: "https://example.com/2"
+    },
+    {
+      title: "Example 3",
+      image: "https://via.placeholder.com/150",
+      link: "https://example.com/3"
+    }
+  ];
+  
+  export default contentData;
