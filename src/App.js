89b5618fd4027317694c1591876c9fadfb8f4379
Hyun Jin, Lee <nagi5924@gmail.com>
2023-04-06 20:28:35 +0900

    임시저장

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import "./App.css";
+import List from './component/List';
+import ListMobile from './component/ListMobile'
+
+import { useState } from 'react';
+import styled from "styled-components";
+
+const TestBox = styled.div`
+position: absolute;
+top:0;
+right: 0;
+width: 40px;
+height: 65px;
+background-color: goldenrod;
+display: flex;
+align-items: center;
+`
 
 function App() {
+
+    const [isHover, setIsHover] = useState(false);
+
     return (
         <div className="App">
-            <div>여기는 일단 냅두기 영역</div>
+            {/* <div>여기는 일단 냅두기 영역</div> */}
+
+            <TestBox 
+            onMouseEnter={()=>{setIsHover(!isHover)}}
+            >TestBox</TestBox>
+            {isHover
+            ?<List />
+            :<ListMobile />}
+            <hr/>
+            <div>Test</div>
+            <hr/>
         </div>
     );
 }
