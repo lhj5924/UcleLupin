9d3e5886ab23a0adf94cc2c1c39dbaced5da33f0
Hyun Jin, Lee <nagi5924@gmail.com>
2023-04-06 20:32:48 +0900

    변경사항 롤백

--- src/Globalstyle.js
+++ src/Globalstyle.js
@@ -19,7 +19,6 @@ const GlobalStyles = createGlobalStyle`
   body{
       line-height: 1;
       font-family: 'Noto Sans KR', sans-serif;
-      background-color: cornflowerblue;
       margin-bottom: 100px;
   }
   ol, ul{
