669a8f2e8d98d179d761693604b215deba8efeb1
dd-stack <117844745+dd-stack@users.noreply.github.com>
2023-04-06 18:14:15 +0900

    헤더 전체적인 구성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./component/header.jsx";
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <div>여기는 일단 냅두기 영역</div>
         </div>
     );
