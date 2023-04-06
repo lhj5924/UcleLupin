5e5fb86ca5fcf269e7999ad7faf59ba2e0a3fe00
kijiwon <119961147+kijiwon@users.noreply.github.com>
2023-04-06 20:09:33 +0900

    찐최종

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import TeamPage from './page/TeamPage';
 
 function App() {
     return (
         <div className="App">
             {/* <div>여기는 일단 냅두기 영역</div> */}
-            <TeamPage/>
         </div>
     );
 }
